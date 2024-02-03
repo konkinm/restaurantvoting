2cda1893cfb3a44c00cf1ed6c3d09a81fd7296f1
Max Konkin <mirk.mad@gmail.com>
2024-02-03 14:24:02 +0400

    fix: voting time fix

--- src/main/java/ru/konkin/restaurantvoting/web/user/VoteController.java
+++ src/main/java/ru/konkin/restaurantvoting/web/user/VoteController.java
@@ -33,7 +33,7 @@ import static ru.konkin.restaurantvoting.web.RestValidation.checkNew;
 public class VoteController {
     static final String REST_URL = "/api/profile/votes";
 
-    private static final LocalTime VOTE_TIME_LIMIT = LocalTime.of(14, 0);
+    private static final LocalTime VOTE_TIME_LIMIT = LocalTime.of(11, 0);
 
     @Autowired
     private VoteRepository voteRepository;
