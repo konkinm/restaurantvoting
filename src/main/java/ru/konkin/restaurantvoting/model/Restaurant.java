ca6eee2acb457fe83ada31cbcb6fbee54a77d5b1
Max Konkin <mirk.mad@gmail.com>
2024-02-03 14:23:00 +0400

    refactor: unique name field introduced

--- src/main/java/ru/konkin/restaurantvoting/model/Restaurant.java
+++ src/main/java/ru/konkin/restaurantvoting/model/Restaurant.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import ru.konkin.restaurantvoting.HasId;
 import ru.konkin.restaurantvoting.View;
+import ru.konkin.restaurantvoting.validation.NoHtml;
 
 import java.util.List;
 import java.util.Set;
@@ -21,7 +24,14 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Restaurant extends NamedEntity implements HasId {
+public class Restaurant extends BaseEntity implements HasId {
+    @NotBlank
+    @Size(min = 2, max = 128)
+    @Column(name = "name", nullable = false, unique = true)
+    @NoHtml
+    @JsonView(View.BasicInfo.class)
+    private String name;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")//, cascade = CascadeType.REMOVE, orphanRemoval = true)
     @OrderBy("description ASC")
     @OnDelete(action = OnDeleteAction.CASCADE) //https://stackoverflow.com/a/44988100/548473
@@ -35,4 +45,14 @@ public class Restaurant extends NamedEntity implements HasId {
     @JsonView(View.AdditionalInfo.class)
     @Schema(hidden = true)
     private Set<Vote> votes;
+
+    public Restaurant(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + '[' + name + ']';
+    }
 }
